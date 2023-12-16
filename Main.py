4aa9da419695f7951ca35cc4ef89f1b4b70d134d
Bren Guzmán <125343617+BrenGuzmanB@users.noreply.github.com>
2023-12-15 23:54:00 -0600

    Mapas de calor y Clasificación

--- Main.py
+++ Main.py
@@ -9,6 +9,9 @@ import pandas as pd
 import numpy as np
 from corr import calcular_correlaciones, matriz_correlacion
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 #%% Cargar el conjunto de datos
 data = pd.read_csv('penguins_size.csv')
@@ -46,3 +49,80 @@ columnas_numericas = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm'
 df_correlaciones = calcular_correlaciones(data, columnas_numericas)
 print(f"\nTabla de Correlaciones:\n{df_correlaciones}")
 
+#%% Matriz de correlación (Mapa de calor): Spearman
+
+columnas_numericas = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex_encoded', 'island_encoded', 'species_encoded']
+
+# Calcular la matriz de correlación
+
+correlation_matrix = matriz_correlacion(data[columnas_numericas], metodo='spearman')
+
+# Crear un mapa de calor
+plt.figure(figsize=(10, 8))
+heatmap = plt.pcolor(correlation_matrix, cmap='coolwarm', vmin=-1, vmax=1)
+
+# Anotar los valores de correlación en los cuadros
+for i in range(len(correlation_matrix.index)):
+    for j in range(len(correlation_matrix.columns)):
+        plt.text(j + 0.5, i + 0.5, f'{correlation_matrix.iloc[i, j]:.2f}', ha='center', va='center', color='black')
+
+plt.title('Matriz de correlación (Mapa de calor): Spearman')
+plt.xticks(np.arange(0.5, len(correlation_matrix.columns), 1), correlation_matrix.columns, rotation=45)
+plt.yticks(np.arange(0.5, len(correlation_matrix.index), 1), correlation_matrix.index)
+plt.colorbar(heatmap)
+
+plt.show()
+
+#%% Matriz de correlación (Mapa de calor): Kendall
+
+columnas_numericas = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm', 'body_mass_g', 'sex_encoded', 'island_encoded', 'species_encoded']
+
+# Calcular la matriz de correlación
+
+correlation_matrix = matriz_correlacion(data[columnas_numericas], metodo='kendall')
+#correlation_matrix = data[columnas_numericas].corr(method='kendall')
+
+# Crear un mapa de calor
+plt.figure(figsize=(10, 8))
+heatmap = plt.pcolor(correlation_matrix, cmap='coolwarm', vmin=-1, vmax=1)
+
+# Anotar los valores de correlación en los cuadros
+for i in range(len(correlation_matrix.index)):
+    for j in range(len(correlation_matrix.columns)):
+        plt.text(j + 0.5, i + 0.5, f'{correlation_matrix.iloc[i, j]:.2f}', ha='center', va='center', color='black')
+
+plt.title('Matriz de correlación (Mapa de calor): Kendall')
+plt.xticks(np.arange(0.5, len(correlation_matrix.columns), 1), correlation_matrix.columns, rotation=45)
+plt.yticks(np.arange(0.5, len(correlation_matrix.index), 1), correlation_matrix.index)
+plt.colorbar(heatmap)
+
+plt.show()
+
+#%% Clasificación (Random Forest)
+
+cols = ['culmen_length_mm', 'culmen_depth_mm', 'flipper_length_mm']
+
+
+# Dividir los datos en características (X) y variable objetivo (y)
+X = data[cols]  
+y = data['species_encoded']
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3)
+
+# Inicializar el clasificador de Bosques Aleatorios
+clf = RandomForestClassifier(random_state=1998)
+
+# Entrenar el modelo
+clf.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred = clf.predict(X_test)
+
+# Evaluar el rendimiento del modelo
+accuracy = accuracy_score(y_test, y_pred)
+print("\n\nPrecisión del modelo:", accuracy)
+
+# Matriz de Confusión
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("\nMatriz de Confusión:\n", conf_matrix)
